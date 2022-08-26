dba9a83628794e00bd11cdd45ff2cc0fb99c00e4
Cierra Hilliary <103157123+chillsan99@users.noreply.github.com>
2022-08-26 15:12:13 -0400

    Add files via upload

---
+++ words.js
@@ -0,0 +1,127 @@
+let words = [
+    {
+        word: "bulbasaur",
+        hint: "Grass and Poison Type Starter Pokemon",
+        No: "001"
+    },
+    {
+        word: "Pikachu",
+        hint: "The Pokemon everyone knows and loves",
+        No: "025"
+    },
+        {
+        word: "charmander",
+        hint: "Fire type starter Pokemon",
+        No: "004"
+    },
+    {
+        word: "charizard",
+        hint: "Fire and Flying type that everyone wanted",
+        No: "006"
+    },
+    {
+        word: "squirtle",
+        hint: "Water type starter Pokemon",
+        No: "008"
+    },
+    {
+        word: "metapod",
+        hint: "Bug Type that sheds skin",
+        No:"011"
+    },
+    {
+        word: "Kakuna",
+        hint: "Bug and Poison Type that sheds its skin",
+        No: "014"
+    },
+    {
+        word: "Beedrill",
+        hint: "Insect Pokémon which mostly resembles a yellow wasp",
+        No: "015"
+    },
+    {
+        word: "Pidgey",
+        hint: " A normal and flying type small, plump-bodied avian Pokémon",
+        No: "016"
+    },
+    {
+        word: "Pidgeotto",
+        hint: "Also a normal and flying type  raptor-like avian Pokemon ",
+        No: "016"
+    },
+    {
+        word: "Rattata",
+        hint: "Just a normal rat",
+        No: "014"
+    },
+    {
+        word: "Raticate",
+        hint: "The large, rodent-like Pokémon",
+        No: "014"
+    },
+    {
+        word: "Ekans",
+        hint: "I'm the same backwards as I am forward",
+        No: "023"
+    },
+    {
+        word: "Arbok",
+        hint: "I'm also the same backwards as I am forward",
+        No: "024"
+    },
+    {
+        word: "Diglett",
+        hint: "Ability of tongue to detect flavour",
+        No: "050"
+    },
+    {
+        word: "Meowth",
+        hint: "The best member of Team Rocket",
+        No: "052"
+    },
+    {
+        word: "Persian",
+        hint: "The Classy Cat Pokémon",
+        No: "053"
+    },
+    {
+        word: "Psyduck",
+        hint: "That ONE duck pokemon",
+        No: "054"
+    },
+    {
+        word: "Growlithe",
+        hint: "Orange fur with black stripes along its back and legs",
+        No: "058"
+    },
+    {
+        word: "Poliwag",
+        hint: "Amphibian Pokémon resembling a blue, spherical tadpole",
+        No: "060"
+    },
+    {
+        word: "Ivysaur",
+        hint: "Evolves from Bulbasaur",
+        No: "002"
+    },
+    {
+        word: "Venusaur",
+        hint: "A squat, quadrupedal amphibian Pokémon with bumpy, blue-green skin",
+        No: "003"
+    },
+    {
+        word: "Charmeleon",
+        hint: "Has a vicious nature and will constantly seek out opponents",
+        No: "005"
+    },
+    {
+        word: "Blastoise",
+        hint: "The Shellfish Pokémon also known as Kamex (カメックス)",
+        No: "009"
+    },
+    {
+        word: "Wartortle",
+        hint: "Known as Kameil (カメール) in Japan!",
+        No: "008"
+    },
+]
